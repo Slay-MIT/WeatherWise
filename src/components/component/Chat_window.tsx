adf5b410fdc3aacb711697d8781dd0bd6fe64196
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-07-17 10:16:33 +0530

    icon changes

--- src/components/component/Chat_window.tsx
+++ src/components/component/Chat_window.tsx
@@ -77,10 +77,13 @@ export function Chat_window() {
   return (
     <div className="flex flex-col h-[600px] bg-dark-blue-800 w-full rounded-lg shadow-lg overflow-hidden font-lekton">
       <header className="flex items-center gap-4 px-6 py-4 bg-dark-blue-700">
-        <Avatar className="w-10 h-10 border-2 border-primary">
-          <AvatarImage src="/placeholder-user.jpg" />
-          <AvatarFallback>WW</AvatarFallback>
-        </Avatar>
+      <Avatar className="w-10 h-10 border-2 border-primary items-center justify-center">
+        {/* <AvatarFallback> */}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+          </svg>
+        {/* </AvatarFallback> */}
+      </Avatar>
         <div className="text-lg font-medium text-foreground">WeatherWise</div>
       </header>
       <div ref={chatContainerRef} className="flex-1 overflow-auto p-6 bg-dark-blue-800">
@@ -91,7 +94,11 @@ export function Chat_window() {
                 {message.type === 'user' ? (
                   <AvatarFallback>U</AvatarFallback>
                 ) : (
-                  <AvatarFallback>WW</AvatarFallback>
+                  <AvatarFallback>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                      <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                    </svg>
+                  </AvatarFallback>
                 )}
               </Avatar>
               <div className={`grid gap-1 ${message.type === 'bot' ? 'bg-dark-blue-700' : 'bg-primary'} p-4 rounded-lg max-w-[80%]`}>
@@ -108,7 +115,11 @@ export function Chat_window() {
           {isTyping && (
             <div className="flex items-start gap-4">
               <Avatar className="w-8 h-8 border border-primary">
-                <AvatarFallback>WW</AvatarFallback>
+                <AvatarFallback>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                  </svg>
+                </AvatarFallback>
               </Avatar>
               <div className="bg-dark-blue-700 p-4 rounded-lg">
                 <div className="font-medium text-foreground">WeatherWise</div>
